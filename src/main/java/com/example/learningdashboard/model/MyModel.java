b94fbe8985b2d4bff927cc06113c7ec2dbc916e6
calandula <36953570+calandula@users.noreply.github.com>
2023-04-03 02:14:59 +0200

    init

---
+++ src/main/java/com/example/learningdashboard/model/MyModel.java
@@ -0,0 +1,14 @@
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+public class MyModel {
+
+    private Model model;
+
+    public MyModel() {
+        model = ModelFactory.createDefaultModel();
+    }
+
+    // Add methods for manipulating the model
+
+}
---
+++ src/main/java/com/example/learningdashboard/service/MyService.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    // Add methods for implementing business logic
+
+}
