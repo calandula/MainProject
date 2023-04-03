6118d475d29a2d0cd2f8412deeae7184c5fdf61b
calandula <36953570+calandula@users.noreply.github.com>
2023-04-03 02:18:12 +0200

    init

---
+++ src/main/java/com/example/learningdashboard/LearningdashboardApplication.java
@@ -0,0 +1,14 @@
+package com.example.learningdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningdashboardApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningdashboardApplication.class, args);
+	}
+
+}
